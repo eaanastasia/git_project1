f472ab25b3fc01a5178f6e3f5a99325c32c2dd7f
Анастасия <62378851+eaanastasia@users.noreply.github.com>
2020-11-16 17:41:44 +0300

    Update program.py
    33

--- program.py
+++ program.py
@@ -2,7 +2,8 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('Commit 3')
+    print('New changes on GitHub')
 
-
+    
 if __name__ == '__main__':
     main()
